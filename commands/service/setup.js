3eb53ae8d1b17368fdc246ba7e76e75896d36a53
DAMXITZ ' S <bsknightgamer@hotmail.com>
2026-02-14 15:15:02 +0700

    Update setup.js

--- commands/service/setup.js
+++ commands/service/setup.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const config = require('../../config.json');
 const { SelectRow } = require('../../Modal/selectmenu');
+const { rowBtn } = require('../../Modal/button');
 
 module.exports = {
 	name: 'setup',
@@ -21,26 +22,6 @@ module.exports = {
 			.setThumbnail('https://media.discordapp.net/attachments/1472104110844285061/1472132616760070197/35353535353535.png?ex=699175d6&is=69902456&hm=dd56e290ee9fa398a9b66611a6bf5f35636898460d49bf7a6fa553d6ad3b7b13&=&format=webp&quality=lossless&width=350&height=350')
 			.setColor("#999999")
 			.setTimestamp();
-
-		const url = new Discord.ButtonBuilder()
-			.setURL("https://docs.google.com/spreadsheets/d/1Zh72uTnluCI-YRKOd4adygM5dg2MC5ivMkKpJLtKSrc/edit#gid=0")
-			.setLabel("เช็คลาปกติ")	
-			.setEmoji('<:281761:1472133628006891662>')
-			.setStyle(Discord.ButtonStyle.Link);
-
-		const url2 = new Discord.ButtonBuilder()
-			.setURL("https://docs.google.com/spreadsheets/d/11F1Dmro2MQrdHV0aYeZuL4zmfIPVbkuJthW9U8hsSFk/edit#gid=0")
-			.setLabel("เช็คลาทุกวัน")
-			.setEmoji('<:281761:1472133628006891662>')
-			.setStyle(Discord.ButtonStyle.Link);
-			
-		const url3 = new Discord.ButtonBuilder()
-			.setURL("https://www.facebook.com/xtencrud.derek/")
-			.setLabel("ระบบทั้งหมดทำโดย")	
-			.setEmoji('<:fb:1424467238848696330>')
-			.setStyle(Discord.ButtonStyle.Link);
-		
-		const rowBtn = new Discord.ActionRowBuilder().addComponents(url, url2, url3);
 
 		i.channel.send({
 			embeds: [embed],
